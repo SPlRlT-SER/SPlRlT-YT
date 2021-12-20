9557605725b8a613eab87d933841a67314319f87
SPlRlT SER <95764723+SPlRlT-SER@users.noreply.github.com>
2021-12-20 22:21:59 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Miss Helena By Rafi💖*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*SPlRlT YT By SPlRlT💖*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
